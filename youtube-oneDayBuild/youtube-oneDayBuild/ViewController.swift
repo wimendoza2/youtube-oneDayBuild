973e9a135a000b88c5e53f47ae2ed6d67cf2901a
Wilmer Mendoza <wil13ale@hotmail.com>
2022-02-01 22:17:35 -0400

    creando el proyecto

---
+++ youtube-oneDayBuild/youtube-oneDayBuild/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  youtube-oneDayBuild
+//
+//  Created by Wilmer Mendoza on 1/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
